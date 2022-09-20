b857980f90bb96556f9963772eb2e2f6ac81f4fe
drewspampy22 <drewspampy22@gmail.com>
2022-09-19 20:06:31 -0400

    commit

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,22 @@ public class Main
 {
     public static void main(String[] args)
     {
+        double ItemCost = 3.50;
+        double ShipingCost = 0;
+        double TotalCost = 0;
 
+        if (ItemCost >= 100)
+        {
+           ShipingCost=0;
+        }
+        else
+        {
+            ShipingCost= ItemCost * 0.02;
+        }
+
+        TotalCost = ItemCost+ ShipingCost;
+            System.out.println("Shipping cost is " + ShipingCost);
+            System.out.println("Total cost is " + TotalCost);
     }
 
 }
